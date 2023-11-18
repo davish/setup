952debe5adeb193a171d301196d58638b919d7f6
Davis Haupt <me@davishaupt.com>
2023-11-18 01:46:56 -0500

    initial commit

---
+++ darwin.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }: 
+
+{
+    # List packages installed in system profile. To search by name, run:
+    # $ nix-env -qaP | grep wget
+    environment.systemPackages =
+    [ pkgs.vim ];
+
+    # Auto upgrade nix package and the daemon service.
+    services.nix-daemon.enable = true;
+    services.karabiner-elements.enable = true;
+    # nix.package = pkgs.nix;
+
+    # Necessary for using flakes on this system.
+    nix.settings.experimental-features = "nix-command flakes";
+
+    # Create /etc/zshrc that loads the nix-darwin environment.
+    programs.zsh.enable = true;  # default shell on catalina
+    # programs.fish.enable = true;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 4;
+
+    system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+
+    # The platform the configuration will be used on.
+    nixpkgs.hostPlatform = "aarch64-darwin";
+
+    users.users.davish = {
+        name = "davish";
+        home = "/Users/davish";
+    };
+
+    homebrew.enable = true;
+    homebrew.casks = [
+        "visual-studio-code"
+    ];
+    homebrew.brews = [];
+}
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#davish-desktop
+    darwinConfigurations."davish-desktop" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+         ./darwin.nix
+
+         home-manager.darwinModules.home-manager {
+             home-manager.useGlobalPkgs = true;
+             home-manager.useUserPackages = true;
+             home-manager.users.davish = import ./home.nix;
+         }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."davish-desktop".pkgs;
+  };
+}
