491dabab3c6af773b332759830692cd9493210c6
Davis Haupt <me@davishaupt.com>
2023-11-26 20:18:50 -0500

    cleanup

--- home-manager.nix
+++ home-manager.nix
@@ -16,22 +16,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
     pkgs.tmux
   ];
 
@@ -77,6 +61,7 @@
       gp = "git push";
       gpup = "git push -u origin $(git rev-parse --abbrev-ref HEAD)";
       gst = "git status";
+      gd = "git diff";
 
       drs = "darwin-rebuild switch --flake ~/.config/nix";
 
