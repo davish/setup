780aa67ec9155d12b46d12f90bbe136a70b3a9ea
Davis Haupt <me@davishaupt.com>
2023-12-11 21:49:56 -0500

    some doom updates

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -74,3 +74,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(after! projectile
+  (when (file-exists-p "~/.projectile.el") (load "~/.projectile.el")))
