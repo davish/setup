4c1896d548ef2c3dbf4f6c22d2878382300a93a7
Davis Haupt <davis.haupt@gmail.com>
2024-02-11 11:41:24 -0500

    zsh: go up/down through history

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -11,6 +11,7 @@
     config = {
         programs.zsh = {
             enable = true;
+
             shellAliases = {
                 rrc = "source ~/.zshenv && source ~/.zshrc";
                 sw = "darwin-rebuild switch --flake ~/.config/nix";
@@ -26,6 +27,17 @@
                 gd   = "git diff";
                 gs   = "git switch";
             };
+
+            initExtra = ''
+                # Search through history with prefix that's already typed.
+                # https://unix.stackexchange.com/questions/621606/zsh-completion-with-up-and-down-arrows
+                autoload -U up-line-or-beginning-search
+                autoload -U down-line-or-beginning-search
+                zle -N up-line-or-beginning-search
+                zle -N down-line-or-beginning-search
+                bindkey "^[[A" up-line-or-beginning-search
+                bindkey "^[[B" down-line-or-beginning-search
+            '';
 
             envExtra = lib.strings.concatMapStrings (p: "export PATH=$PATH:$HOME/${p}\n") config.shell.paths;
         };
