02456a843c5461d3bcf4333162a942b8a9dd77d7
Davis Haupt <davis.haupt@gmail.com>
2024-02-11 11:23:33 -0500

    improve DSL for scripts

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,9 +1,26 @@
-{ ... }:
+{ lib, ... }:
 
+let 
+    mapKey = f: attrs: lib.attrsets.concatMapAttrs (name: value: {"${(f name)}" = value;}) attrs;
+
+    linkScripts = scripts: 
+        mapKey (name: "scripts/${name}") 
+            (lib.attrsets.genAttrs scripts (name: {
+                source = ./${name};
+                executable = true;
+            }));
+in
 {
-    home.file."scripts" = {
-        source = ./scripts;
-    };
+    home.file = 
+        {
+            "scripts/git-stack-scripts" = {
+                source = ./git-stack-scripts;
+            };
+        } // linkScripts [
+            "freeport"
+            "git-stack"
+            "hello"
+        ];
 
     shell.paths = [
         "scripts"
