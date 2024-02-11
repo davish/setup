62d23a1bcd15af720348f45621f6e7c6e3436f49
Davis Haupt <davis.haupt@gmail.com>
2024-02-11 11:33:22 -0500

    reformatting

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,26 +1,25 @@
 { lib, ... }:
 
 let 
-    mapKey = f: attrs: lib.attrsets.concatMapAttrs (name: value: {"${(f name)}" = value;}) attrs;
+    mapKeys = f: attrs: lib.attrsets.concatMapAttrs (name: value: {"${(f name)}" = value;}) attrs;
 
     linkScripts = scripts: 
-        mapKey (name: "scripts/${name}") 
+        mapKeys (name: "scripts/${name}") 
             (lib.attrsets.genAttrs scripts (name: {
                 source = ./${name};
                 executable = true;
             }));
 in
 {
-    home.file = 
-        {
-            "scripts/git-stack-scripts" = {
-                source = ./git-stack-scripts;
-            };
-        } // linkScripts [
-            "freeport"
-            "git-stack"
-            "hello"
-        ];
+    home.file = linkScripts [
+        "freeport"
+        "git-stack"
+        "hello"
+    ] // {
+        "scripts/git-stack-scripts" = {
+            source = ./git-stack-scripts;
+        };
+    };
 
     shell.paths = [
         "scripts"
